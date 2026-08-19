6a7f0f1c5af621524e26ba41df29e986a7d1e0df
joyce162 <13638645701@163.com>
2026-08-19 10:35:11 +0800

    add file

--- demo.py
+++ demo.py
@@ -1,3 +1,6 @@
+import functools
+
+
 def print_line():
     print("-"*20)
 
@@ -58,3 +61,56 @@ print(find_all(li))
 def min_max(*args):
     return max(args),min(args)
 print(min_max(1,2,3,4,5))
+
+def sum_nums(num):
+    if num==1:
+        return 1
+    return num+sum_nums(num-1)
+
+print(sum_nums(5))
+
+fn = lambda a,b:a+b
+print(fn(1,2))
+
+fn2 = lambda :100
+print(fn2())
+
+fn3 = lambda a,b:a if a>b else b
+print(fn3(1,2))
+
+def sum_num(a,b,f):
+    return f(a)+f(b)
+print(sum_num(-1,-4,abs))
+
+
+list1=[1,2,3,4,5,6,7,8,9]
+def func(x):
+    return x%2==0
+result = filter(func,list1)
+print(list(result))
+
+li1 = [1,2,3,4,5,6]
+def func4(x):
+    return x%2 != 0
+li4 = list(filter(func4,li1))
+def func1(a,b):
+    return a+b
+result1 = functools.reduce(func1, li4)
+print(result1)
+
+li2 = [1,2,None,3,None,5]
+def func2(x):
+    return x != None
+result2 = filter(func2,li2)
+print(list(result2))
+
+li3 = [1,2,3,4,5,6]
+def func3(x):
+    if x % 2 == 0:
+        return x*2
+    else:
+        return x
+result3 = map(func3,li3)
+print(list(result3))
+
+
